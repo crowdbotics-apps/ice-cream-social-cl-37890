eeaa4b674b0ef1fad19841afce1a7031dcc3f55b
Crowdbotics <team@crowdbotics.com>
2022-12-14 01:10:34 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ice_cream_social_cl_37890/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ice_cream_social_cl_37890;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "ice_cream_social_cl_37890";
+  }
+}
