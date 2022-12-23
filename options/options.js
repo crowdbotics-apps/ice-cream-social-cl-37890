18cde6632b3b2962ae42cab74e58d5c592e38671
ivanapfafsovljanski <ivana.pfaf@crowdbotics.com>
2022-12-23 17:24:49 +0100

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "ice_cream_social_cl_37890",
   url: "https://ice_cream_social_cl_37890.botics.co",
   api: "https://ice_cream_social_cl_37890.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
